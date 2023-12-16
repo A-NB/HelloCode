5ab2ced3a74e0e05ebab96c1c0af495869f96576
A-NB <bel-an510@yandex.ru>
2023-12-16 14:14:47 +0300

    added Example013 TwoDimArray_&_RecursionAlgorythm

--- Example002_HelloUser/Program.cs
+++ Example002_HelloUser/Program.cs
@@ -1,4 +1,17 @@
-﻿Console.Write("Введите своё имя: ");
+﻿/* System.Console.InputEncoding = Encoding.GetEncoding(1251);
+
+Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+var enc1251 = Encoding.GetEncoding(1251);
+ 
+System.Console.OutputEncoding = System.Text.Encoding.UTF8;
+System.Console.InputEncoding = enc1251; 
+
+// https://www.cyberforum.ru/csharp-beginners/thread2388539.html
+
+*/
+Console.InputEncoding = System.Text.Encoding.GetEncoding("utf-16");  // РАБОТАЕТ!!!
+
+Console.Write("Введите своё имя: ");
 string username = Console.ReadLine();
 Console.Write("Привет,  ");
 Console.Write(username);
---
+++ Example013_TwoDimArray_and_RecursionAlgorythm/Program.cs
@@ -0,0 +1,128 @@
+﻿// string[,] table = new string[2, 5];
+// table[1, 2] = "слово";
+
+// for (int rows = 0; rows < 2; rows++)
+// {
+//     for (int columns = 0; columns < 5; columns++)
+//     {
+//         System.Console.WriteLine($"-{table[rows, columns]}-");
+//     }
+// }
+
+/* void FillArray(int[,] matr)
+{
+    for (int rows = 0; rows < matr.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < matr.GetLength(1); columns++)
+        {
+            matr[rows, columns] = new Random().Next(1, 10);  // [1; 10)
+        }
+    }
+}
+
+
+void PrintArray(int[,] matr)
+{
+    for (int rows = 0; rows < matr.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < matr.GetLength(1); columns++)
+        {
+            System.Console.Write($"{matr[rows, columns]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] matrix = new int[3, 4];
+PrintArray(matrix);
+FillArray(matrix);
+System.Console.WriteLine();
+PrintArray(matrix); */
+
+/* void PrintImage(int[,] image)
+{
+    for (int rows = 0; rows < image.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < image.GetLength(1); columns++)
+        {
+            // System.Console.Write($"{image[rows, columns]} ");
+            if (image[rows, columns] == 0) System.Console.Write($" ");
+            else System.Console.Write($"+");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] pic = new int[,]
+{
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0},
+    {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+    {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+    {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0},
+    {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+    {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+    {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0},
+    {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
+    {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
+    {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0},
+    {0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+};
+
+void FillImage(int row, int column)
+{
+    if (pic[row, column] == 0)
+    {
+        pic[row, column] = 1;
+        FillImage(row - 1, column);
+        FillImage(row, column - 1);
+        FillImage(row + 1, column);
+        FillImage(row, column + 1);
+    }
+}
+
+Console.Clear();
+FillImage(13, 13);
+PrintImage(pic); */
+
+
+/* double Factorial(int n)
+{
+    // 1! = 1
+    // 0! = 1
+    if (n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+
+// System.Console.WriteLine(Factorial(5));
+
+for (int i = 1; i < 40; i++)
+{
+    System.Console.WriteLine($"{i}! = {Factorial(i)}");
+} */
+
+// f(1) = 1
+// f(2) = 1
+// f(n) = f(n - 1) + f(n -2)
+
+double Fibonacci(int n)
+{
+    if(n ==1 || n ==2) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n -2);
+}
+
+for (int i = 1; i < 50; i++)
+{
+    System.Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}
